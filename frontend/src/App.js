f298c4b56b70a82c0a514f84360e420b0cc84e62
Oscar Olivares <oscarolivares625@gmail.com>
2019-08-17 22:20:31 -0400

    Trabajando en los modulos del frontend con material-ui

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+//import logo from './logo.svg';
+//import './App.css';
+import { Switch, Route } from 'react-router-dom';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Login from './components/session/Login';
+import SignUp from './components/session/SignUp';
+import PasswordRecovery from './components/session/PasswordRecovery';
+
+import Home from './components/onepirate/Home';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <CssBaseline />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/login" component={Login} />
+        <Route path="/signup" component={SignUp} />
+        <Route path="/password-reovery" component={PasswordRecovery} />
+      </Switch>
+    </>
   );
 }
 
---
+++ frontend/src/components/session/PasswordRecovery.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function PasswordRecovery() {
+  return (
+    <div>
+      <h2>Password Recovery Module</h2>
+    </div>
+  );
+}
